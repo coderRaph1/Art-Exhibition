27b20d126855111781fe18415d357fbfacfdf0da
CoderRaph1 <raphael02@hotmail.co.uk>
2025-02-25 11:02:34 +0000

    Chnages to PORT

--- server.js
+++ server.js
@@ -21,7 +21,9 @@ app.use('/api/', collectionsRoutes)
 app.use(notFound)
 app.use(errorHandler)
 
-const PORT = process.env.PORT || (process.env.NODE_ENV === 'test' ? 3001 : 3000)
+// const PORT = process.env.PORT || (process.env.NODE_ENV === 'test' ? 3001 : 3000)
+
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
