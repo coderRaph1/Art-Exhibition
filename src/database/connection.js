bcf7b8ba1d1b492abd2d8ad4c2de69051250c160
CoderRaph1 <raphael02@hotmail.co.uk>
2025-03-24 10:31:12 +0000

    removed duplicates of pool

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,15 +1,15 @@
-const { Pool } = require("pg")
-require("dotenv").config()
+// const { Pool } = require("pg")
+// require("dotenv").config()
 
-const isProduction = process.env.NODE_ENV === "production"
-const connectionString = isProduction ? process.env.DATABASE_URL : process.env.DB_URL
+// const isProduction = process.env.NODE_ENV === "production"
+// const connectionString = isProduction ? process.env.DATABASE_URL : process.env.DB_URL
 
-console.log("Connecting to database:", connectionString) 
+// console.log("Connecting to database:", connectionString) 
 
-const pool = new Pool({
-  connectionString,
-  ssl: isProduction ? { rejectUnauthorized: false } : false,
-})
+// const pool = new Pool({
+//   connectionString,
+//   ssl: isProduction ? { rejectUnauthorized: false } : false,
+// })
 
 // module.exports = pool
 
